048fd74f427ec9e10cc47547eb2efdbbae6c8f35
sruthidatla <sruthidatla@missionrnd.com>
2016-03-09 13:56:27 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -12,5 +12,22 @@ ERROR CASES: Return -1 in error cases.
 NOTES:
 */
 int count(int num1, int num2) {
-	return 0;
+	int coun = 0, i;
+	if (num1 == 0)
+		return -1;
+    if (num1 == 1)
+		return num2;
+	else if (num1<=num2)
+	{
+		for (i = num1; i <= num2; i++)
+		{
+			if ((i%num1) == 0)
+				coun++;
+
+		}
+		return coun;
+		
+	}
+	else
+      return 0;
 }
